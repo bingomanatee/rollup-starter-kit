1398035222b483bc94287aef97c820dcd4605698
Edelhart.Dave <dave.edelhart@mckesson.com>
2021-03-27 11:02:23 -0700

    v. local config includes

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,28 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
+import sizes from 'rollup-plugin-sizes';
+
+const plugins = [
+  resolve(),
+  commonjs(),
+  terser(),
+  sizes(),
+];
+if (process.env.NODE_ENV === 'test') {
+  console.log('un-terse for testing')
+  plugins.splice(2, 1);
+}
+
+const pkg = require('./package.json')
+const name = pkg.name.replace('-', '_');
 
 module.exports = {
   input: 'src/index.js',
-  plugins: [
-    resolve(),
-    commonjs(),
-    terser(),
-  ],
+  plugins,
   output: {
     file: 'lib/index.js',
     format: 'umd',
-    name: 'LGE',
+    name: name,
   },
 };
